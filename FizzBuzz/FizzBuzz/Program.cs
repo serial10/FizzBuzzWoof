b444e07444f7d312abb110b6384c455a00db7752
Greg <adams86_05@hotmail.com>
2017-07-15 18:40:56 -0500

    Add files via upload

---
+++ FizzBuzz/FizzBuzz/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace FizzBuzz
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {           
+            for (int i = 1; i <= 1000; i++)
+            {
+                if (i % 3 == 0)
+                {                    
+                    if (i % 3 == 0 && i % 5 == 0 && i % 7 == 0)
+                    {
+                        Console.WriteLine("{0} " + "Fizz " + "Buzz " + "Woof", i);
+                        continue;
+                    }
+                    if (i % 3 == 0 && i % 7 == 0)
+                    {
+                        Console.WriteLine("{0} " + "Fizz " + "Woof", i);
+                        continue;
+                    }
+                    if (i % 3 == 0 && i % 5 == 0)
+                    {
+                        Console.WriteLine("{0} " + "Fizz " + "Buzz", i);
+                        continue;
+                    }
+                        Console.WriteLine("{0} " + "Fizz", i);
+                        continue;                 
+                }
+                if (i % 5 == 0)
+                {
+                    if (i % 5 == 0 && i % 7 == 0)
+                    {
+                        Console.WriteLine("{0} " + "Buzz " + "Woof", i);
+                        continue;
+                    }
+                        Console.WriteLine("{0} " + "Buzz", i);
+                        continue;                    
+                }
+                if (i % 7 == 0)
+                {
+                    Console.WriteLine("{0} " + "Woof", i);
+                    continue;
+                }
+                    Console.WriteLine(i);                
+            }
+        }
+    }
+}
